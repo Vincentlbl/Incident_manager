7ad4e3275c0dea8790427a5a46ddf3f7df6a1ac3
VinzODL <145387121+VinzODL@users.noreply.github.com>
2024-10-19 21:43:44 +0200

    Mise à jour des chemins dans menu.php

--- src/view/menu.php
+++ src/view/menu.php
@@ -1,13 +1,13 @@
 <nav class="bg-gray-800 p-4">
     <div class="container mx-auto flex justify-between items-center">
-        <a href="/projet_php/public/" class="text-white text-lg font-bold">Accueil</a>
+        <a href="/Incident_manager-main/public/" class="text-white text-lg font-bold">Accueil</a>
         <h1 class="text-white text-2xl font-bold mx-auto">Projet PHP</h1>
         <ul class="flex space-x-4">
             <li>
-                <a href="/projet_php/public/incidents" class="text-gray-300 hover:text-white">Liste des incidents</a>
+                <a href="/Incident_manager-main/public/incidents" class="text-gray-300 hover:text-white">Liste des incidents</a>
             </li>
             <li>
-                <a href="/projet_php/public/add" class="text-gray-300 hover:text-white">Ajouter un incident</a>
+                <a href="/Incident_manager-main/public/add" class="text-gray-300 hover:text-white">Ajouter un incident</a>
             </li>
         </ul>
     </div>
